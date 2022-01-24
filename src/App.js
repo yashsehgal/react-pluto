12269ad70b3f8591518127342e6ff5704842dd47
Yash Sehgal <yashsehgal.work@gmail.com>
2022-01-24 15:46:02 +0000

    Test/Feat: export for text-component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Text } from './lib';
+import './lib/components/scss/index.css';
 
 export default function App() {
     return (
         <div className="app">
-            App is running...
+            <Text type="body" variant="body-lg">
+                React Pluto Components (Design System + UI Kit)
+            </Text>
+            <Text type="text" variant="text-md">
+                A package of small but beautiful React components from the planet Pluto.
+            </Text>
         </div>
     )
 }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,12 @@
 import Button from './components/Button';
+import Text from './components/Typography/Text';
+
 
 // css styles and variables
 import './components/scss/index.css';
 
 // exporting all the components
 export {
-    Button
+    Button,
+    Text
 }
