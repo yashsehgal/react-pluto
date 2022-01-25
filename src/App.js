f097c750124d660e42592cc1e8b4e7fb612364e4
Yash Sehgal <yashsehgal.work@gmail.com>
2022-01-25 07:43:56 +0000

    Chore: Box component properties added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { Heading, Text } from './lib';
+import { Heading, Text, Box } from './lib';
 import './lib/components/scss/index.css';
 
 export default function App() {
     return (
         <div className="app">
-            <Heading variant="4xl"
-                style={{ textAlign: 'center' }}
+            <Box className="hero-section"
+                onClick={() => console.log("working...")}
             >
-                React Pluto Components
-            </Heading>
-            <Text type="body" variant="body-lg"
-                style={{ textAlign: 'center' }}
-            >
-                React Pluto Components (Design System + UI Kit)
-            </Text>
-            <Text type="text" variant="text-md"
-                style={{ textAlign: 'center' }}
-            >
-                A package of small but beautiful React components from the planet Pluto.
-            </Text>
+                <Heading variant="4xl"
+                    style={{ textAlign: 'center' }}
+                >
+                    React Pluto Components
+                </Heading>
+                <Text type="body" variant="body-lg"
+                    style={{ textAlign: 'center' }}
+                >
+                    React Pluto Components (Design System + UI Kit)
+                </Text>
+                <Text type="text" variant="text-md"
+                    style={{ textAlign: 'center' }}
+                >
+                    A package of small but beautiful React components from the planet Pluto.
+                </Text>
+            </Box>
         </div>
     )
 }
--- src/lib/components/Box.js
+++ src/lib/components/Box.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function Box({ children }, componentProps) {
+export default function Box({ children, className, style, id, onClick }) {
+    const boxComponentProperties = {
+        "style": style,
+        "className": (className) ? className : '',
+        "id": (id) ? id : '',
+        "onClick": onClick
+    };
+
     return (
         <React.Fragment>
-            <div {...componentProps}>
+            <div
+                style={boxComponentProperties.style}
+                className={boxComponentProperties.className}
+                id={boxComponentProperties.id}
+                onClick={boxComponentProperties.onClick}
+            >
                 {children ? children : ''}
             </div>
         </React.Fragment>
